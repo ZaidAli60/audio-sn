770aaa98d183aeea3125d4fb87ab8c901c8c25f5
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2024-04-28 22:57:15 +0500

    design verification page

---
+++ src/pages/Authentication/Verification.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FiArrowLeft } from "react-icons/fi";
+import video from 'assets/video/vid_sub.mp4'
+import axios from 'axios';
+import { Button } from 'antd';
+
+const SERVER_URL = process.env.REACT_APP_API_END_POINT
+const initialState = { email: "" }
+
+export default function Verification() {
+    const [state, setState] = useState(initialState);
+    const [isProcessing, setIsProcessing] = useState(false)
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setState({ ...state, [e.target.name]: e.target.value })
+    }
+
+    const handleResetPassword = e => {
+        e.preventDefault()
+        let { email } = state
+        setIsProcessing(true)
+
+        axios.post(`${SERVER_URL}/api/forgot-password`, { email })
+            .then(res => {
+                let { data, status } = res
+                if (status === 200) {
+                    window.toastify(data.message, "success")
+                }
+                setState({})
+                setIsProcessing(false)
+            })
+            .catch(err => {
+                window.toastify(err.response?.data?.error || "Something went wrong, please try again", "error")
+            })
+            .finally(() => {
+                setIsProcessing(false)
+            })
+    }
+
+    return (
+        <div className='login'>
+            <div className="mb-0 d-flex overflow-hidden max-vh-100">
+                <div className="bg-black min-vh-100 text-white p-2 p-md-5 d-flex align-items-center left-signin" style={{ width: '500px', height: '100vh' }}>
+                    <div className='w-100'>
+                        <h4>Verification
+                        </h4>
+                        <p className='m-0 p-0 mb-4' style={{ color: '#90998b' }}>Enter your verification code.</p>
+                        <div className='input-form'>
+                            <div className='floating-label-content'>
+                                <input className='floating-input' value={state.email} onChange={handleChange} name='email' type='text' placeholder=' ' />
+                                <label className='floating-label'>Code</label>
+                            </div>
+                            <Button type="primary" className="custom-btn w-100" size='large' shape='round' onClick={handleResetPassword} loading={isProcessing}>Verify</Button>
+                            <Link to="/auth" style={{ color: '#90998b' }} className='text-decoration-underline hover-text'><FiArrowLeft size={20} className='me-2' />Return to Log in</Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="p-0 m-0 d-none d-md-block h-full" style={{ flexGrow: 1, height: '100vh' }}>
+                    <video src={video} autoPlay muted loop playsInline controls={false} style={{ width: "100%", objectFit: "cover", height: "100%" }}  >
+                    </video>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Authentication/index.js
+++ src/pages/Authentication/index.js
@@ -4,6 +4,7 @@ import Login from './Login'
 import ForgotPassword from './ForgotPassword'
 import Register from './Register'
 import RestPassword from './RestPassword'
+import Verification from './Verification'
 
 export default function index() {
     return (
@@ -13,6 +14,7 @@ export default function index() {
                 <Route path='register' element={<Register />} />
                 <Route path='forgot-password' element={<ForgotPassword />} />
                 <Route path='rest-password' element={<RestPassword />} />
+                <Route path='verification' element={<Verification />} />
             </Route>
         </Routes>
     )
